ad82028a14f75ffd5bcabc94435ccedb70ad2107
Basav C H <basuch463@gmail.com>
2026-04-25 21:44:08 +0530

    added multer middleware, Openrouter setup done, created api for Analyzing resume

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
+import interviewRouter from "./routes/interview.route.js";
 dotenv.config();
 
 const app = express();
@@ -20,6 +21,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
+app.use("/api/interview",interviewRouter)
 
 const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
